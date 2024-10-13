85a0289d935c55f4bae2daa490d82bf7d419b0f8
root <root@homesrv03.local>
2024-10-13 19:03:01 +0200

    add srv03

--- 23_k3s_node2.sh
+++ 23_k3s_node2.sh
@@ -5,25 +5,22 @@ set -o xtrace
 # to add a node
 #
 
-export MASTER1=vmi1053342.contaboserver.net
-export MASTER2=vmi1026786.contaboserver.net
-export MASTER3=vmi1026787.contaboserver.net
+export MASTER_IP=10.0.0.1
+export K3S_TOKEN=HomeDCK3sToken
 
-export MASTER_IP=10.0.0.3
-export K3S_TOKEN=LeanSysK3sToken
-
-NODE_EXTERNAL_IP=38.242.150.11
-NODE_INTERNAL_IP=10.0.0.2
-INTERNAL_INTERFACE=eth1
+NODE_EXTERNAL_IP=192.168.68.203
+NODE_INTERNAL_IP=10.0.0.3
+INTERNAL_INTERFACE=enp3s0
 
 #
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.23.5+k3s1 K3S_TOKEN="${K3S_TOKEN}" sh -s server \
+curl -sfL https://get.k3s.io | INSTALL_K3S_VERSION=v1.31.1+k3s1 K3S_TOKEN="${K3S_TOKEN}" sh -s server \
 --server https://${MASTER_IP}:6443 \
 --node-external-ip=${NODE_EXTERNAL_IP}  \
 --node-ip=${NODE_INTERNAL_IP} \
 --advertise-address=${NODE_INTERNAL_IP} \
 --flannel-iface=${INTERNAL_INTERFACE}
 
+sleep 5
 kubectl get nodes -o wide
 
