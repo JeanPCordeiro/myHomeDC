32d0b7efdaee50a1cd49a80297df2b89f2c3ab28
root <root@homesrv01.local>
2024-10-13 21:23:58 +0200

    longhorn

---
+++ 30_longhorn.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -o xtrace
+
+#export KUBECONFIG=/etc/k3s.yaml
+
+#
+# Install Longhorn
+#
+kubectl apply -f https://raw.githubusercontent.com/longhorn/longhorn/v1.7.1/deploy/longhorn.yaml
+watch kubectl get pods -n longhorn-system
+sleep 5
+kubectl create -f https://raw.githubusercontent.com/longhorn/longhorn/v1.7.1/examples/storageclass.yaml
+kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"false"}}}'
+kubectl patch storageclass longhorn -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
+
+kubectl get sc
